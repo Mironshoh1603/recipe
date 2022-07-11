8d7074e3dec96afa57eb09b5a47d7cd87b572524
Mironshoh1603 <mironshohasadov1603.gmail.com>
2022-07-12 02:02:41 +0500

    add cool face Api

--- app.js
+++ app.js
@@ -29,6 +29,6 @@ app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
 
 const routes = require("./server/routes/recipeRoutes.js");
-app.use("/", routes);
+app.use("/", routes).get("/cool", (req, res) => res.send(cool()));
 
 app.listen(port, () => console.log(` ${port} tinglanmoqda !!!`));
