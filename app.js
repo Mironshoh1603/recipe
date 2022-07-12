150bc823504f70c962d7f61997335a59344f42cf
Mironshoh1603 <mironshohasadov1603.gmail.com>
2022-07-12 09:27:01 +0500

    ef

--- app.js
+++ app.js
@@ -7,8 +7,28 @@ const session = require("express-session");
 const cookieParser = require("cookie-parser");
 const flash = require("connect-flash");
 
+const { Pool } = require("pg");
+const pool = new Pool({
+  connectionString: process.env.DATABASE.raplace(
+    "<password>",
+    process.env.PASSWORD
+  ),
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
 const app = express();
 const port = process.env.PORT || 3000;
+
+showTimes = () => {
+  let result = "";
+  const times = process.env.TIMES || 5;
+  for (i = 0; i < times; i++) {
+    result += i + " ";
+  }
+  return result;
+};
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -29,6 +49,21 @@ app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
 
 const routes = require("./server/routes/recipeRoutes.js");
-app.use("/", routes).get("/cool", (req, res) => res.send(cool()));
+app
+  .use("/", routes)
+  .get("/cool", (req, res) => res.send(cool()))
+  .get("/times", (req, res) => res.send(showTimes()))
+  .get("/db", async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query("SELECT * FROM test_table");
+      const results = { results: result ? result.rows : null };
+      res.render("pages/db", results);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  });
 
 app.listen(port, () => console.log(` ${port} tinglanmoqda !!!`));
--- server/models/database.js
+++ server/models/database.js
@@ -1,6 +1,13 @@
 const mongoose = require("mongoose");
 const db = process.env.DATABASE.replace("<password>", process.env.PASSWORD);
 mongoose.connect(db, {}).then(() => console.log("DATABASE connnected.."));
+const { Pool } = require("pg");
+const pool = new Pool({
+  connectionString: db,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 // Models
 require("./Category");
